7c0550d8cda86413aa0d78fbf44dfe1926795b65
sowislo06 <julius.wessing95@gmail.com>
2017-06-16 17:00:49 +0200

    Scaffold Projekt mit Verlinkung

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :projects
   devise_for :users
   get 'welcome/index'
 
   root 'welcome#index'
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616132444) do
+ActiveRecord::Schema.define(version: 20170616134942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.date "dueDate"
+    t.date "startDate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
