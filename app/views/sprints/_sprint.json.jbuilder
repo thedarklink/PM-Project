f2ae8e8a7d3f3f260bba39579ff326841bf33ffd
sowislo06 <julius.wessing95@gmail.com>
2017-07-13 19:08:49 +0200

    Sprint anlegen

---
+++ app/views/sprints/_sprint.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! sprint, :id, :name, :startDate, :endDate, :effort, :remainingWork, :created_at, :updated_at
+json.url sprint_url(sprint, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616134942) do
+ActiveRecord::Schema.define(version: 20170713160247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170616134942) do
     t.date "startDate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sprint_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -33,6 +34,17 @@ ActiveRecord::Schema.define(version: 20170616134942) do
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
+  create_table "sprints", force: :cascade do |t|
+    t.string "name"
+    t.date "startDate"
+    t.date "endDate"
+    t.integer "effort"
+    t.integer "remainingWork"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "project_id"
   end
 
   create_table "users", force: :cascade do |t|
